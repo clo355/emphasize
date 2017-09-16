bc52522a697c7770835cf6237afba9fa1d00c03a
clo355 <lo.christopher.355@gmail.com>
2017-09-16 00:21:58 -0400

    Implemented rename-file option for listView longClick

--- app/src/main/java/com/cl/emphasize/MainActivity.java
+++ app/src/main/java/com/cl/emphasize/MainActivity.java
@@ -1,6 +1,7 @@
 package com.cl.emphasize;
 
 import android.annotation.TargetApi;
+import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AlertDialog;
@@ -11,11 +12,15 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -23,11 +28,11 @@ import java.util.ArrayList;
 /**********************************************************************************
  *   Emphasize
  *
- *   This app allows you post notes onto your home screen, and emphasize them
- *   by having them flash or jiggle. Your notes are saved as actual
- *   text files.
+ *   This app allows you post notes onto your home screen, and emphasize them with
+ *   blinking or jiggling. Your notes are saved as actual text files.
  *
- *   Inspired by Colornote!
+ *   Core features modeled after ColorNote and Microsoft Word.
+ *
  *   CL
  **********************************************************************************/
 
@@ -118,12 +123,12 @@ public class MainActivity extends AppCompatActivity {
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener(){
             @Override
             public boolean onItemLongClick(AdapterView<?> adapterView, View view, int position, long l){
-                CharSequence options[] = new CharSequence[] {"Edit", "Delete", "Properties"};
+                CharSequence options[] = new CharSequence[] {"Edit", "Rename", "Delete", "Properties"};
                 final File longClickedFile = new File(getFilesDir(), myFileNameArray.get(position));
-                final String optionsFileName = longClickedFile.getName();
+                final String longClickedFileName = longClickedFile.getName();
 
                 AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder.setTitle(optionsFileName);
+                builder.setTitle(longClickedFileName);
                 builder.setItems(options, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int clickedOption) {
@@ -147,14 +152,73 @@ public class MainActivity extends AppCompatActivity {
                                 startActivity(newFileIntent);
                                 break;
                             }
-                            case 1: { //Delete
+                            case 1: { //Rename
+                                final EditText renameEditText = new EditText(MainActivity.this);
+                                DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener(){
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which){
+                                        //Create an EditText for user input
+                                        LinearLayout.LayoutParams renameLayout = new LinearLayout.LayoutParams(
+                                                LinearLayout.LayoutParams.MATCH_PARENT,
+                                                LinearLayout.LayoutParams.MATCH_PARENT);
+                                        renameEditText.setLayoutParams(renameLayout);
+
+                                        switch (which){
+                                            case DialogInterface.BUTTON_POSITIVE:{ //OK
+                                                //also need to check if file exists or not
+
+                                                //Get contents of old file
+                                                String oldFileContents = "";
+                                                try {
+                                                    BufferedReader fileReader = new BufferedReader(new FileReader(longClickedFile));
+                                                    String line;
+                                                    while((line = fileReader.readLine()) != null) {
+                                                        oldFileContents = oldFileContents + line + "\n";
+                                                    }
+                                                } catch(IOException e){
+                                                    Log.d("MAIN", "IOException");
+                                                }
+
+                                                //fill new file with old file's contents
+                                                FileOutputStream myOutputStream;
+                                                File newFile = new File(getFilesDir(), renameEditText.getText().toString());
+                                                try{
+                                                    myOutputStream = new FileOutputStream(newFile, false);
+                                                    myOutputStream.write(oldFileContents.getBytes());
+                                                    myOutputStream.close();
+                                                } catch(FileNotFoundException e){
+                                                    Log.d("SAVEAS", "FileNotFoundException");
+                                                } catch(IOException e) {
+                                                    Log.d("SAVEAS", "IOException");
+                                                }
+                                                File oldFile = new File(getFilesDir(), longClickedFileName);
+                                                oldFile.delete();
+                                                updateListView();
+                                                break;
+                                            }
+                                            case DialogInterface.BUTTON_NEGATIVE:{ //Cancel
+                                                break;
+                                            }
+                                        }
+                                    }
+                                };
+
+                                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                                builder.setMessage("Rename file:")
+                                        .setView(renameEditText)
+                                        .setPositiveButton("OK", dialogClickListener)
+                                        .setNegativeButton("Cancel", dialogClickListener)
+                                        .show();
+                                break;
+                            }
+                            case 2: { //Delete
                                 DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
                                     @Override
                                     public void onClick(DialogInterface dialog, int which) {
                                         switch (which){
                                             case DialogInterface.BUTTON_POSITIVE: {
-                                                File oldFile = new File(getFilesDir(), optionsFileName);
-                                                oldFile.delete();
+                                                File fileToDelete = new File(getFilesDir(), longClickedFileName);
+                                                fileToDelete.delete();
                                                 updateListView();
                                                 break;
                                             }
@@ -166,13 +230,13 @@ public class MainActivity extends AppCompatActivity {
                                 };
 
                                 AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                                builder.setMessage("Delete " + optionsFileName + "?")
+                                builder.setMessage("Delete " + longClickedFileName + "?")
                                         .setPositiveButton("Delete", dialogClickListener)
                                         .setNegativeButton("Cancel", dialogClickListener)
                                         .show();
                                 break;
                             }
-                            case 2: { //Properties: show create date, last edit date, file size
+                            case 3: { //Properties: show create date, last edit date, file size
                             }
                         }
                     }
